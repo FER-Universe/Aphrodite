e244ae4e5a0878f42afffec14925d49560e8da56
kdhht2334 <kdhht5022@gmail.com>
2023-12-24 22:05:15 +0900

    feat: update db router

--- backend/apis/version1/database_router.py
+++ backend/apis/version1/database_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from apis.data.database import query_db
+from apis.data.database import add_message_to_database, query_info_to_database
 from apis.data.models import Employee
 
 router = APIRouter(
@@ -9,9 +9,17 @@ router = APIRouter(
 )
 
 
+@router.get("/add")
+def add_db(message: str):
+    add_message_to_database(message=message)
+    return {"message": "finished to add info to database!"}
+
+
 @router.get("/query")
-def query_to_database():
-    results = query_db(table_name="Employee", filter={"Education": "Masters"})
+def query_db():
+    results = query_info_to_database(
+        table_name="Employee", filter={"Education": "Masters"}
+    )
     return results
 
 
--- backend/apis/version1/openai_router.py
+++ backend/apis/version1/openai_router.py
@@ -4,23 +4,24 @@ from threading import Semaphore
 
 import openai
 import requests
-from configs.config import settings
 from fastapi import APIRouter
+
+from apis.data.database import add_message_to_db
+from configs.config import settings
 from schemas.gpt_sch import GptRequestSch, GptResponseSch, GptResponseSch_msgonly
 
 sem = Semaphore(3)
-
 router = APIRouter(prefix="/api/openai")
 
 
 @router.post("/translate/by/gpt", response_model=GptResponseSch_msgonly)
 async def translate_by_gpt_router(req: GptRequestSch):
     openai_result = await translate_by_openai(req)
-    # bard_result = await translate_by_bard(req)
+
+    add_message_to_db(openai_result)
 
     return {
         "openai_msg_ctt": openai_result,
-        # "bard_msg_ctt": bard_result,
     }
 
 
@@ -28,7 +29,6 @@ async def translate_by_openai(req: GptRequestSch):
     with sem:
         data = {
             "model": "gpt-3.5-turbo",
-            # "messages": [{"role": "user", "content": req.title_nm + "한국어로 100자 이내로 설명해줘"}],
             "messages": [
                 {"role": "user", "content": req.title_nm + "한국어로 100자 이내로 말해 줘."}
             ],
@@ -47,9 +47,3 @@ async def translate_by_openai(req: GptRequestSch):
         time.sleep(1)
 
     return openai_result
-
-
-# async def translate_by_bard(req: GptRequestSch):
-#     bard = Bard(token=settings.BARD_API_KEY)
-#     bard_result = bard.get_answer(req.title_nm + '한국어로 100자 이내로 설명해줘')['content']
-#     return bard_result
