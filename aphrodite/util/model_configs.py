895697776cd3238205e33619de40a5a5e44255a1
kdhht2334 <kdhht5022@gmail.com>
2023-12-28 16:52:24 +0900

    feat: aphrodite/feat/sdxl_with_lora

--- aphrodite/util/model_configs.py
+++ aphrodite/util/model_configs.py
@@ -1,15 +1,20 @@
 MODEL_CONFIG = {
-    "pipe_opt": {
-        "torch.dtype": "torch.float16",
-        "variant": "fp16",
-        "device": "cpu",
-    },
-    "inference_opt": {
-        "num_inference_steps": 25,
-        "guidance_scale": 7.0,
-    },
-    "sdxl-realistic": {
+    # default configs
+    "device": "cpu",  # cuda | mps | etc.
+    "max_token_length": 77,
+    "clip_skip": 2,  # or 1
+    "manual_seed": 1234,
+    # model configs
+    "sdxl-hello-world": {
         "type": "name",
-        "name": "",
+        "name": "/path/to/XXX.safetensors",
+        "pipe_opt": {
+            "torch.dtype": "torch.float16",
+            "variant": "fp16",
+            "device": "cpu",
+            "safety_checker": None,
+        },
+        "inference_opt": {"num_inference_steps": 25, "guidance_scale": 7.0},
+        "lora_path": "/path/to/XXX.safetensors",
     },
 }
